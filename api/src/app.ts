9a73ed4cc4ce862bb56db01bfb9ad586e0f13062
claudioares <claudiojasoares.dev@gmail.com>
2024-05-27 01:42:55 -0300

    feat: Adicionando url expecifica para chamadas na api

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,34 +1,34 @@
-import fastify, { FastifyInstance } from "fastify"
+import fastify, { FastifyInstance } from "fastify";
 import cors from "@fastify/cors";
 import { ListProducts } from "./routers/list.products";
 import { CreteProducts } from "./routers/create.products";
 import { DeleteProducts } from "./routers/delete.products";
 import { EditProduct } from "./routers/edit.product";
 
-
 export class App {
-    private app: FastifyInstance;
-    constructor() {
-        this.app = fastify()
-    }
+  private app: FastifyInstance;
+  constructor() {
+    this.app = fastify();
+  }
 
+  listen() {
+    this.app
+      .listen({
+        host: "0.0.0.0",
+        port: process.env.PORT ? Number(process.env.PORT) : 3333,
+      })
+      .then(() => console.log("HTTP Server running..."));
+  }
 
-    listen(){
-        this.app.listen({
-            host: '0.0.0.0',
-            port: process.env.PORT ? Number(process.env.PORT) : 3333,
-        }).then(()=>console.log("HTTP Server running..."));
-    };
+  register() {
+    this.app.register(cors, {
+      origin: "https://challenge-alura-geek-theta.vercel.app/",
+      methods: ["PUT", "POST", "DELETE", "GET"],
+    });
 
-    register(){
-        this.app.register(cors, {
-            origin: "*",
-            methods: ['PUT', 'POST', 'DELETE', 'GET']
-        })
-
-        this.app.register(ListProducts)
-        this.app.register(CreteProducts)
-        this.app.register(DeleteProducts)
-        this.app.register(EditProduct)
-    }
+    this.app.register(ListProducts);
+    this.app.register(CreteProducts);
+    this.app.register(DeleteProducts);
+    this.app.register(EditProduct);
+  }
 }
